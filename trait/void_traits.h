87b49ab9d3d0a6c23b123cba85c772d6d4c9921c
magnus <Magnusbackyard@live.com>
2016-04-22 20:54:37 +0800

    `void` type curryings

--- trait/void_traits.h
+++ trait/void_traits.h
@@ -13,46 +13,74 @@
 namespace Promise2 {
 	namespace VoidTrait {
 		
-	  template<typename ReturnType, typename ArgType>
-	  struct VoidTypeWrapper {
-	    static auto currying(std::function<ReturnType(ArgType)>&& f) {
-	      return std::move(f);
-	    }
-	  };
+		// return type
+		template<typename ReturnType>
+		struct ReturnVoid {
+			template<typename... Args>
+			static auto currying(std::function<ReturnType(Args...)>&& f) {
+				return std::move(f);
+			}
+		};
 
-	  template<typename ArgType>
-	  struct VoidTypeWrapper<void, ArgType> {
-	    static auto currying(std::function<void(ArgType)>&& f) {
-	      auto fn = [rawFn = std::move(f)](ArgType arg) {
-	        rawFn(std::forward<ArgType>(arg));
+		template<>
+		struct ReturnVoid<void> {
+			template<typename... Args>
+			static auto currying(std::function<ReturnType(Args...)>&& f) {
+				auto fn = [rawFn = std::move(f)](Args... args) {
+	        rawFn(std::forward<ArgType...>(args...));
 	        return Void{};
 	      };
 
-	      return std::function<Void(ArgType)>{ std::move(fn) };
-	    }
-	  };
+	      return std::function<Void(Args...)>{ std::move(fn) };
+			}
+		};
 
-	  template<typename ReturnType>
-	  struct VoidTypeWrapper<ReturnType, void> {
-	    static auto currying(std::function<ReturnType(void)>&& f) {
-	      auto fn = [rawFn = std::move(f)](Void) {
-	        return std::forward<ReturnType>(rawFn());
+		// args type
+		template<typename... Types>
+		struct LastType {
+			using Type = typename LastType<Types...>::Type;
+		};
+
+		template<typename T>
+		struct LastType<T> {
+			using Type = T;
+		};
+
+		template<typename T>
+		struct LastTypeOfFunction {};
+
+		template<typename ReturnType, typename Args...>
+		struct LastTypeOfFunction<std::function<ReturnType(Args...)>> {
+			using Type = typename LastType<Args...>::Type;
+		};
+
+		template<typename ReturnType>
+		struct LastTypeOfFunction<std::function<ReturnType(void)>> {
+			using Type = void;
+		};
+
+		template<typename Fn, typename T>
+		struct LastArgTypeIs : public std::is_same<LastTypeOfFunction<Fn>::Type, T> {};
+
+		template<typename ArgType>
+		struct ArgVoid {
+			template<typename ReturnType, typename... Args>
+			static auto currying(std::function<ReturnType(Args...)>&& f) {
+				return std::move(f);
+			}
+		};
+
+		template<>
+		struct ArgVoid<void> {
+			// return type can not be void
+			template<typename ReturnType, typename... Args>
+			static auto currying(std::function<ReturnType(Args...)>&& f) {
+ 				auto fn = [rawFn = std::move(f)](Args... args, Void) {
+	        return std::forward<ReturnType>(rawFn(args...));
 	      };
 
-	      return std::function<ReturnType(Void)>{ std::move(fn) };
-	    }
-	  };
-
-	  template<>
-	  struct VoidTypeWrapper<void, void> {
-	    static auto currying(std::function<void(void)>&& f) {
-	      auto fn = [rawFn = std::move(f)](Void) {
-	        rawFn();
-	        return Void{};
-	      };
-
-	      return std::function<Void(Void)>{ std::move(fn) };
-	    }
-	  };
+				return std::function<ReturnType(Args..., Void)>{ std::move(fn) };
+			}
+		};
 	} // VoidTrait
 }
